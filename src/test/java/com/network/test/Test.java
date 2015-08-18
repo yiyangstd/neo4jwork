0952830e88b8ce6375df927c1dc0e1d3a82551b6
yiyangstd <yangyistd@163.com>
2015-08-18 16:56:39 +0800

    Initial commit

---
+++ src/test/java/com/network/test/Test.java
@@ -0,0 +1,11 @@
+package com.network.test;
+
+/**
+ * Created by Administrator on 2015/8/18.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
